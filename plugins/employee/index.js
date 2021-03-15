a5ee84a011558e9c4c7aaceb645dab0631b48728
akash72 <https://gitlab.com/akash72/leave-management>
2021-03-15 13:52:12 +0530

    schema changes

--- plugins/employee/index.js
+++ plugins/employee/index.js
@@ -1,22 +1,22 @@
 'use strict';
-const  { getEmployeeIdByUserId } = require('./methods/getEmployeeId');
-const { getUserIdByEmployeeId } = require('./methods/getUserId');
+const {getEmployeeIdByUserId} = require('./methods/getEmployeeId');
+const {getUserIdByEmployeeId} = require('./methods/getUserId');
 
 exports.employeePlugin = {
-    name: 'Employee',
-    register: async (server,options) =>{
-        try {
-            server.route(require('./routes/post_employee'));
-            server.route(require('./routes/get_employee'));
-            server.route(require('./routes/put_employee'));
-            server.route(require('./routes/delete_employee'));
-            server.route(require('./routes/get_all_employees'));
-            server.route(require('./routes/get_emp_by_dept'));
-            server.route(require('./routes/reset_password'));
-            server.method('getEmployeeIdByUserId',getEmployeeIdByUserId);
-            server.method('getUserIdByEmployeeId',getUserIdByEmployeeId);
-        } catch (e) {
-            throw e;
-        }
+  name: 'Employee',
+  register: async (server, options) =>{
+    try {
+      server.route(require('./routes/post_employee'));
+      server.route(require('./routes/get_employee'));
+      server.route(require('./routes/put_employee'));
+      server.route(require('./routes/delete_employee'));
+      server.route(require('./routes/get_all_employees'));
+      server.route(require('./routes/get_emp_by_dept'));
+      server.route(require('./routes/reset_password'));
+      server.method('getEmployeeIdByUserId', getEmployeeIdByUserId);
+      server.method('getUserIdByEmployeeId', getUserIdByEmployeeId);
+    } catch (e) {
+      throw e;
     }
-}
+  },
+};
--- server.js
+++ server.js
@@ -1,78 +1,78 @@
-const Hapi = require('@hapi/hapi')
+const Hapi = require('@hapi/hapi');
 const prisma = require('./prismaplugin/prisma');
 const Inert = require('@hapi/inert');
 const Vision = require('@hapi/vision');
 const HapiSwagger = require('hapi-swagger');
 const Pack = require('./package');
 const {employeePlugin} = require('./plugins/employee/index');
-const { authPlugin } = require('./plugins/auth/index');
+const {authPlugin} = require('./plugins/auth/index');
 const {leavePlugin} = require('./plugins/apply_leave/index');
 const {managerPlugin} = require('./plugins/leave_management/manager_leave/index');
 const {hrPlugin} = require('./plugins/leave_management/hr_leave/index');
-const { departmentPlugin } = require('./plugins/department');
+const {departmentPlugin} = require('./plugins/department');
 
 const server = Hapi.server({
-    port: 3000,
-    // host: 'localhost',
+  port: 3000,
+  // host: 'localhost',
 });
 
 const init = async (server) => {
-    const swaggerOptions = {
-        info: {
-                title: 'Leave Management API Documentation',
-                version: Pack.version,
-            },
-            securityDefinitions:{
-                Bearer: {
-                    type: 'apiKey',
-                    name: 'Authorization',
-                    in: 'header',
-                    'x-keyPrefix': 'Bearer'
-                }
-            },
-            security: [{ Bearer: []  }]
-        };
-    await server.register([
-        Inert,
-        Vision,
-        {
-            plugin: HapiSwagger,
-            options: swaggerOptions
-        },authPlugin,
-        {
-            plugin: require('hapi-authorization'),
-            options: {
-                roles: ['HR', 'MANAGER', 'EMPLOYEE']	
-            }
-        },
-        prisma, 
-        departmentPlugin,
-        employeePlugin, 
-        leavePlugin, 
-        managerPlugin, 
-        hrPlugin
-    ]);
-    await server.initialize();
-    await server.route({
-        method:'GET',
-        path: '/',
-        options:{
-            auth:false,
-            description: 'Home Page',
-            notes: 'Welcome page',
-            tags: ['api'],
-        },
-        handler: (request,h)=>{
-            return h.response({up:true});
-        }
-    })
-    await server.start();
-    console.log(`Server running on ${server.info.uri}`);
-    return server;
-}
-process.on('unhandledRejection',(err)=>{
-    console.log(err);
-    process.exit(1);
+  const swaggerOptions = {
+    info: {
+      title: 'Leave Management API Documentation',
+      version: Pack.version,
+    },
+    securityDefinitions: {
+      Bearer: {
+        'type': 'apiKey',
+        'name': 'Authorization',
+        'in': 'header',
+        'x-keyPrefix': 'Bearer',
+      },
+    },
+    security: [{Bearer: []}],
+  };
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions,
+    }, authPlugin,
+    {
+      plugin: require('hapi-authorization'),
+      options: {
+        roles: ['HR', 'MANAGER', 'EMPLOYEE'],
+      },
+    },
+    prisma,
+    departmentPlugin,
+    employeePlugin,
+    leavePlugin,
+    managerPlugin,
+    hrPlugin,
+  ]);
+  await server.initialize();
+  await server.route({
+    method: 'GET',
+    path: '/',
+    options: {
+      auth: false,
+      description: 'Home Page',
+      notes: 'Welcome page',
+      tags: ['api'],
+    },
+    handler: (request, h)=>{
+      return h.response({up: true});
+    },
+  });
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
+  return server;
+};
+process.on('unhandledRejection', (err)=>{
+  console.log(err);
+  process.exit(1);
 });
 
 init(server);
