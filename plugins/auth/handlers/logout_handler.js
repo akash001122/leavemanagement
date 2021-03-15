12a6809766e470d458cdbcacd0670b0b4a0e2311
akash72 <https://gitlab.com/akash72/leave-management>
2021-03-15 23:22:07 +0530

    initial commit

--- plugins/auth/handlers/logout_handler.js
+++ plugins/auth/handlers/logout_handler.js
@@ -1,36 +1,33 @@
-const { promisify } = require("util");
-const redis = require("redis");
+const {promisify} = require('util');
+const redis = require('redis');
 const client = redis.createClient();
 const setAsync = promisify(client.set).bind(client);
 const getAsync = promisify(client.get).bind(client);
 
-client.on("error", function(error) {
-    console.error(error);
-  });
+client.on('error', function(error) {
+  console.error(error);
+});
 
-const logoutHandler = async function(request,h){
-    try{
-        const {tokenId} = request.auth.credentials;
-        const tokenDetails = await getAsync(tokenId);
-        const parsedTokenDetails = JSON.parse(tokenDetails);
-        const credentials = {
-            userId: parsedTokenDetails.userid,
-            employeeId: parsedTokenDetails.employeeid,
-            departmentId: parsedTokenDetails.departmentid,
-            role: parsedTokenDetails.role,
-            isValid: false
-        }
-        await setAsync(tokenId,JSON.stringify(credentials))
-        return{
-            statusCode:200,
-            message: "Logged Out Successfully"
-        }
-        
-    }catch(e){
-        throw e;
-    }
-}
-
-
+const logoutHandler = async function(request, h) {
+  try {
+    const {tokenId} = request.auth.credentials;
+    const tokenDetails = await getAsync(tokenId);
+    const parsedTokenDetails = JSON.parse(tokenDetails);
+    const credentials = {
+      userId: parsedTokenDetails.userid,
+      employeeId: parsedTokenDetails.employeeid,
+      departmentId: parsedTokenDetails.departmentid,
+      role: parsedTokenDetails.role,
+      isValid: false,
+    };
+    await setAsync(tokenId, JSON.stringify(credentials));
+    return {
+      statusCode: 200,
+      message: 'Logged Out Successfully',
+    };
+  } catch (e) {
+    throw e;
+  }
+};
 
 exports.logoutHandler = logoutHandler;
