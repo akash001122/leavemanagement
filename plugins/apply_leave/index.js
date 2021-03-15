9c770d924082f383a695df4a903513f5487cb632
akash72 <https://gitlab.com/akash72/leave-management>
2021-03-15 23:01:02 +0530

    initial commit

--- plugins/apply_leave/index.js
+++ plugins/apply_leave/index.js
@@ -1,22 +1,20 @@
 'use strict';
 
-const { getEmployeeIdByLeaveId } = require('./methods/getEmployeeId');
-
-
+const {getEmployeeIdByLeaveId} = require('./methods/getEmployeeId');
 
 exports.leavePlugin = {
-    name: 'leave',
-    register: async (server,options) =>{
-        try{
-            server.auth.default('jwt');
-            server.route(require('./routes/post_leave'));
-            server.route(require('./routes/get_leave'));
-            server.route(require('./routes/put_leave'));
-            server.route(require('./routes/delete_leave'));
-            server.route(require('./routes/get_all_leaves'));
-            server.method('getEmployeeIdByLeaveId',getEmployeeIdByLeaveId)
-        }catch(e){
-            throw e;
-        }
+  name: 'leave',
+  register: async (server, options) => {
+    try {
+      server.auth.default('jwt');
+      server.route(require('./routes/post_leave'));
+      server.route(require('./routes/get_leave'));
+      server.route(require('./routes/put_leave'));
+      server.route(require('./routes/delete_leave'));
+      server.route(require('./routes/get_all_leaves'));
+      server.method('getEmployeeIdByLeaveId', getEmployeeIdByLeaveId);
+    } catch (e) {
+      throw e;
     }
-}
+  },
+};
--- plugins/auth/index.js
+++ plugins/auth/index.js
@@ -1,56 +1,54 @@
 'use strict';
 
-const jwt = require('jsonwebtoken');
-const { promisify } = require("util");
-const { JWT_ALGORITHM} = require('../../config/config');
-const redis = require("redis");
-const { JWT_SECRET } = require('../../config/config');
+const {promisify} = require('util');
+const {JWT_ALGORITHM} = require('../../config/config');
+const redis = require('redis');
+const {JWT_SECRET} = require('../../config/config');
 const client = redis.createClient();
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
 const getAsync = promisify(client.get).bind(client);
 
-client.on("error", function(error) {
-    console.error(error);
-  });
-const validate = async function (decoded) {
-    try {
-        const userDetails = await getAsync(decoded.tokenId);
-        const parsedUserDetails = JSON.parse(userDetails)
-        const userId = parsedUserDetails.userId;
-        if(parsedUserDetails && parsedUserDetails.isValid){
-            const employeeDetails = await prisma.$queryRaw`SELECT e.id AS employeeid, e.departmentid, u.role FROM public.employee e INNER JOIN public.userlogin u ON u.id = e.userid WHERE e.userid = ${userId}`;
-            const payload = {
-                isValid: true,
-                credentials:{
-                    tokenId: decoded.tokenId,
-                    userId,
-                    role: employeeDetails[0].role
-                }
-            };
-            return payload
-        }else{
-            return{
-                isValid: false, 
-                errorMessage: "Invalid Token"};
-        }
-    } catch (e) {
-        throw e
+client.on('error', function(error) {
+  console.error(error);
+});
+const validate = async function(decoded) {
+  try {
+    const userDetails = await getAsync(decoded.tokenId);
+    const parsedUserDetails = JSON.parse(userDetails);
+    const userId = parsedUserDetails.userId;
+    if (parsedUserDetails && parsedUserDetails.isValid) {
+      const employeeDetails = await prisma.$queryRaw`SELECT e.id AS employeeid, e.departmentid, u.role FROM public.employee e INNER JOIN public.userlogin u ON u.id = e.userid WHERE e.userid = ${userId}`;
+      const payload = {
+        isValid: true,
+        credentials: {
+          tokenId: decoded.tokenId,
+          userId,
+          role: employeeDetails[0].role,
+        },
+      };
+      return payload;
+    } else {
+      return {
+        isValid: false,
+        errorMessage: 'Invalid Token',
+      };
     }
-}
-
-exports.authPlugin = {
-    name: 'auth',
-    register: async (server, options) => {
-            await server.register(require('hapi-auth-jwt2'))
-            await server.auth.strategy('jwt', 'jwt', {
-                  key: JWT_SECRET,
-                  verifyOptions: { algorithms: [JWT_ALGORITHM] },
-                  validate
-            });
-            server.route(require('./routes/login'));
-            server.route(require('./routes/logout'));
-
-    } 
+  } catch (e) {
+    throw e;
+  }
 };
 
+exports.authPlugin = {
+  name: 'auth',
+  register: async (server, options) => {
+    await server.register(require('hapi-auth-jwt2'));
+    await server.auth.strategy('jwt', 'jwt', {
+      key: JWT_SECRET,
+      verifyOptions: {algorithms: [JWT_ALGORITHM]},
+      validate,
+    });
+    server.route(require('./routes/login'));
+    server.route(require('./routes/logout'));
+  },
+};
