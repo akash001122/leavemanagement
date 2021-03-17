3a0cb63ceea909d58430ad5cf25f5b7cb534861f
akash72 <https://gitlab.com/akash72/leave-management>
2021-03-17 14:22:56 +0530

    total leaves

--- plugins/leave_management/manager_leave/handlers/post_leave_status.js
+++ plugins/leave_management/manager_leave/handlers/post_leave_status.js
@@ -5,25 +5,32 @@ const Boom = require('@hapi/boom');
 const leaveHandler = async (request, h) => {
   try {
     const {prisma} = request.server.app;
-    const managerUserId = request.auth.credentials.userId;
-    const managerDepartmentId = await request.server.methods.getDeptIdByUserId(managerUserId);
-    const managerEmployeeId = await request.server.methods.getEmployeeIdByUserId(managerUserId);
     const leaveId = request.params.leaveId;
     const {leaveStatus} = request.payload;
+    const managerUserId = request.auth.credentials.userId;
+    const managerEmployeeId = await request.server.methods.getEmployeeIdByUserId(managerUserId);
     const employeeId = await request.server.methods.getEmployeeIdByLeaveId(leaveId);
-    const employeeDepartmentId = await request.server.methods.getDeptIdByEmployeeId(employeeId);
-    if (managerDepartmentId === employeeDepartmentId && managerEmployeeId !== employeeId) {
-      const createLeave = await prisma.$queryRaw`UPDATE public.leave SET leavestatus= ${leaveStatus}, leaveupdatedtime = ${Date.now()} WHERE id = ${leaveId};`;
-      return {
-        statusCode: 201,
-        message: `LEAVE ${leaveStatus}`,
-        data: {
-          leaveId,
-          leaveType: createLeave.leaveType,
-          from: createLeave.startDate,
-          to: createLeave.endDate,
-        },
-      };
+    if (managerEmployeeId !== employeeId) {
+      const managerDepartmentId = await request.server.methods.getDeptIdByUserId(managerUserId);
+      const employeeDepartmentId = await request.server.methods.getDeptIdByEmployeeId(employeeId);
+      if (managerDepartmentId === employeeDepartmentId) {
+        let createLeave;
+        if (leaveStatus === 'APPROVED') {
+          createLeave = await prisma.$queryRaw`UPDATE public.leave SET leavestatus= ${leaveStatus}, leaveupdatedtime = ${Date.now()}, totalleavesleft = totalleavesleft - 1 WHERE id = ${leaveId};`;
+        } else {
+          createLeave = await prisma.$queryRaw`UPDATE public.leave SET leavestatus= ${leaveStatus}, leaveupdatedtime = ${Date.now()} WHERE id = ${leaveId};`;
+        }
+        return {
+          statusCode: 201,
+          message: `LEAVE ${leaveStatus}`,
+          data: {
+            leaveId,
+            leaveType: createLeave.leaveType,
+            from: createLeave.startDate,
+            to: createLeave.endDate,
+          },
+        };
+      }
     } else {
       return Boom.unauthorized();
     }
